736b54787e4358f05104c43ed713da8afb103fcc
ginaneumar <129331774+ginaneumar@users.noreply.github.com>
2023-04-15 19:45:39 +0200

    Homework and Symbol finished

---
+++ hw1.js
@@ -0,0 +1,3 @@
+function changeText() {
+    document.getElementById("my-paragraph").innerHTML = "The text has changed!";
+  }
---
+++ symbol.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  background(0, 0, 128);
+
+  fill(240, 248, 255);
+
+  ellipse(110, 110, 300, 300);
+  fill(0, 0, 128);
+  ellipse(50, 50, 250, 250);
+
+  fill(256, 256, 256);
+  circle(10, 20, 5);
+  circle(30, 80, 5);
+  circle(100, 20, 5);
+  circle(90, 130, 5);
+  circle(250, 20, 5);
+  circle(130, 80, 5);
+  circle(130, 280, 5);
+  circle(40, 260, 5);
+  circle(250, 260, 5);
+  circle(280, 160, 5);
+}
