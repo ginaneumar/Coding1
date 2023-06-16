2520719b14bcde1f1f6ed73fac0874d08df48ec7
ginaneumar <129331774+ginaneumar@users.noreply.github.com>
2023-06-16 12:20:45 +0200

    finished some intermediate task
    and made some adjustments with the grid, code and some functions so its more accessible

--- my_final_project_gina/script.js
+++ my_final_project_gina/script.js
@@ -1,87 +1,155 @@
-const gridContainer = document.querySelector(".grid-container"); // Select grid container element
-let cards = []; // Array to store the cards
+const gridContainer = document.querySelector(".grid-container");
+const timerElement = document.querySelector("#timer");
+const scoreElement = document.querySelector("#score");
+const difficultyContainer = document.querySelector(".difficulty-container");
+const gameContainer = document.querySelector(".game-container");
+const startButton = document.querySelector("#start-button");
+const restartButton = document.querySelector("#restart-button");
+
+const flipSound = new Audio("./sounds/notification-sound-7062.mp3");
+const collectSound = new Audio("collect_sound.mp3");
+const dealSound = new Audio("./sounds/daisy_wheel_word_processor-29353.mp3");
+const matchingPair = new Audio("./sounds/game-bonus-144751.mp3");
+const gameSound = new Audio(
+  "./sounds/comfort-atmosphere-pleasant-atmosphere-deep-thought-153275.mp3"
+);
+
+gameSound.loop = true; // Enable looping
+
+let cards = [];
 let flippedCards = [];
-
 let isLocked = false;
+let score = 0;
+let timerInterval;
+let startTime;
 
-// Load JSON data and generate cards after data is fetched
-fetch("cards.json")
-  .then((res) => res.json())
-  .then((data) => {
-    cards = [...data, ...data]; // Create card pairs by duplicating the data
-    shuffleCards();
-    generateCards();
-  });
+// Start game button event listener
+startButton.addEventListener("click", startGame);
 
-// Function to shuffle the cards using the Fisher-Yates algorithm, but i got a simpler version
-function shuffleCards() {
-  cards.sort(() => Math.random() - 0.5);
+// Restart button event listener
+restartButton.addEventListener("click", restartGame);
+
+// Load JSON data and start the game after data is fetched
+function loadCardsAndStartGame(numPairs) {
+  fetch("cards.json")
+    .then((res) => res.json())
+    .then((data) => {
+      cards = data.slice(0, numPairs); // Slice the data to match the selected number of pairs
+      cards = [...cards, ...cards]; // Create card pairs by duplicating the data
+      shuffleCards();
+      generateCards(numPairs * 2); // Generate double the number of pairs for the grid
+      startTimer();
+    });
 }
 
-// generate cards in the grid
-function generateCards() {
-  const numCards = 8 * 5;
+// Generate cards in the grid
+function generateCards(numPairs) {
+  const numCards = numPairs * 2;
+  const numColumns = Math.ceil(Math.sqrt(numCards));
+  const numRows = Math.ceil(numCards / numColumns);
 
-  //new div element for cards, and add card class to card element, Store card index as a data attribute
+  gridContainer.innerHTML = ""; // Clear the grid container
+
+  gridContainer.style.gridTemplateColumns = `repeat(auto-fit, 140px)`;
+  gridContainer.style.gridTemplateRows = `repeat(${numRows}, 140px)`;
+
   for (let i = 0; i < numCards && i < cards.length; i++) {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute("data-index", i);
+    if (i >= numPairs) {
+      break; // Stop generating cards after reaching the desired number of pairs
+    }
 
-    // Create front face, add card-font to front face element
-    const frontFace = document.createElement("div");
-    frontFace.classList.add("card-front");
-
-    // Create image element for backside, set image source
-    const frontSideImg = document.createElement("img");
-    frontSideImg.src = "frontside.jpeg"; // Set the image source
-
-    frontFace.appendChild(frontSideImg); // Append the backside image to the front face
-    card.appendChild(frontFace); // Append the front face to the card
-
-    // Create the back face of the card
-    const backFace = document.createElement("div");
-    backFace.classList.add("card-back"); // Add the 'card-back' class to the back face element
-
-    // Create image element for the card icon
-    const backImg = document.createElement("img");
-    backImg.src = cards[i].image; // Set the image source from the JSON data
-
-    backImg.style.visibility = "hidden"; // Hide the backside image initially
-    backImg.style.backgroundColor = "white"; // background color of front face
-
-    backFace.appendChild(backImg); // Append the card icon image to the back face
-    card.appendChild(backFace); // Append the back face to the card
-
-    gridContainer.appendChild(card); // Append the card to the grid container
-
-    // Add event listener to flip the card on click
-    card.addEventListener("click", () => {
-      const index = card.dataset.index; // Get the card index from the data attribute
-      const isFlipped = card.classList.contains("flipped"); // already flipped?
-
-      if (!isFlipped && !isLocked) {
-        // < 2 card can be flipped
-        card.classList.add("flipped");
-        flippedCards.push(card); //add to Array
-
-        setTimeout(() => {
-          backImg.style.visibility = "visible"; // icon visible
-          frontFace.style.transform = "rotateY(180deg)"; //flippen
-
-          if (flippedCards.length === 2) {
-            //by 2 card compare them
-            compareCards();
-          }
-        }, 100);
-      }
-    });
+    const card = createCard(i);
+    gridContainer.appendChild(card);
   }
 }
 
+// Shuffle the cards using the Fisher-Yates algorithm
+function shuffleCards() {
+  for (let i = cards.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+}
+
+// Create a single card element
+function createCard(index) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("data-index", index);
+
+  const frontFace = document.createElement("div");
+  frontFace.classList.add("card-front");
+
+  const frontSideImg = document.createElement("img");
+  frontSideImg.src = "frontside.jpeg";
+  frontFace.appendChild(frontSideImg);
+  card.appendChild(frontFace);
+
+  const backFace = document.createElement("div");
+  backFace.classList.add("card-back");
+
+  const backImg = document.createElement("img");
+  backImg.src = cards[index].image;
+  backImg.style.visibility = "hidden";
+  backImg.style.backgroundColor = "white";
+  backFace.appendChild(backImg);
+  card.appendChild(backFace);
+
+  card.style.visibility = "hidden";
+
+  setTimeout(() => {
+    card.classList.add("deal-animation");
+    card.style.visibility = "visible";
+    setTimeout(() => {
+      card.classList.remove("deal-animation");
+    }, 100);
+
+    const numPairs = cards.length / 2;
+    const animationDuration = numPairs * 100; // Assuming 100ms per card
+
+    dealSound.currentTime = 0; // Reset the current time of the sound
+    dealSound.play(); // Play the deal sound
+
+    setTimeout(() => {
+      dealSound.pause(); // Pause the deal sound after the calculated duration
+    }, animationDuration);
+  }, index * 100);
+
+  return card;
+}
+
+// Event delegation for card clicks
+gridContainer.addEventListener("click", (event) => {
+  const card = event.target.closest(".card");
+  if (!card || isLocked) return;
+
+  const index = card.dataset.index;
+  const isFlipped = card.classList.contains("flipped");
+
+  if (!isFlipped) {
+    flipSound.currentTime = 0; // Reset the current time of the sound
+    flipSound.play(); // Play the flip sound
+
+    card.classList.add("flipped");
+    flippedCards.push(card);
+
+    setTimeout(() => {
+      const backImg = card.querySelector(".card-back img");
+      backImg.style.visibility = "visible";
+      const frontFace = card.querySelector(".card-front");
+      frontFace.style.transform = "rotateY(180deg)";
+
+      if (flippedCards.length === 2) {
+        compareCards();
+      }
+    }, 100);
+  }
+});
+
+// Compare the flipped cards
 function compareCards() {
   if (flippedCards.length === 2) {
-    isLocked = true; // lock other cards
+    isLocked = true;
     const card1 = flippedCards[0];
     const card2 = flippedCards[1];
 
@@ -89,15 +157,18 @@ function compareCards() {
     const symbol2 = cards[parseInt(card2.dataset.index)].name;
 
     if (symbol1 === symbol2) {
+      matchingPair.play();
       setTimeout(() => {
-        card1.innerHTML = ""; // empty card
-        card2.innerHTML = "";
-
-        card1.style.border = "none"; //remove border too so its really empty
-        card2.style.border = "none";
+        clearCard(card1);
+        clearCard(card2);
 
         checkGameEnd();
-        isLocked = false; // remove the lock
+
+        score++;
+
+        updateScore();
+
+        isLocked = false;
       }, 1000);
     } else {
       setTimeout(() => {
@@ -112,26 +183,13 @@ function compareCards() {
   }
 }
 
-// Restart button event listener
-const restartButton = document.querySelector("#restart-button");
-restartButton.addEventListener("click", restartGame);
-
-// Restart the game function
-function restartGame() {
-  gridContainer.innerHTML = ""; // Clear the grid container
-  flippedCards = []; // Reset flipped cards array
-  isLocked = false; // Unlock cards
-
-  // Load JSON data and generate cards after data is fetched
-  fetch("cards.json")
-    .then((res) => res.json())
-    .then((data) => {
-      cards = [...data, ...data]; // Create card pairs by duplicating the data
-      shuffleCards();
-      generateCards();
-    });
+// Clear the card by removing its content
+function clearCard(card) {
+  card.innerHTML = "";
+  card.style.border = "none";
 }
 
+// Flip the card back to the initial state
 function flipCard(card) {
   const frontFace = card.querySelector(".card-front");
   const backImg = card.querySelector(".card-back img");
@@ -143,11 +201,107 @@ function flipCard(card) {
   }, 100);
 }
 
+// Check if the game has ended
 function checkGameEnd() {
   const flippedCards = document.querySelectorAll(".card.flipped");
 
   if (flippedCards.length === cards.length) {
-    // Das Spiel ist beendet
-    console.log("Spiel beendet!");
+    console.log("Game Over!");
+    stopTimer();
+    // winScreen();
   }
 }
+
+
+// function winScreen() {
+//   const winScreen = document.querySelector("#win-screen");
+//   winScreen.style.display = "block";
+
+//   // Add event listener to the "Play Again" button
+//   const playAgainButton = document.querySelector("#restart-button-win");
+//   playAgainButton.addEventListener("click", restartGame);
+// }
+
+// Update the score element
+function updateScore() {
+  scoreElement.textContent = `Score: ${score}`;
+}
+
+// Start the timer
+function startTimer() {
+  startTime = new Date().getTime();
+
+  timerInterval = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const elapsedTime = currentTime - startTime;
+
+    const minutes = Math.floor(elapsedTime / 60000);
+    const seconds = Math.floor((elapsedTime % 60000) / 1000);
+
+    timerElement.textContent = `${formatTime(minutes)}:${formatTime(seconds)}`;
+  }, 1000);
+}
+
+// Format the time with leading zeros
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
+
+// Stop the timer
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerElement.textContent = "00:00";
+}
+
+// Start the game with the selected difficulty
+function startGame() {
+  const difficultySelect = document.querySelector("#difficulty");
+  const selectedDifficulty = difficultySelect.value;
+  let numPairs;
+
+  switch (selectedDifficulty) {
+    case "easy":
+      numPairs = 15;
+      break;
+    case "medium":
+      numPairs = 20;
+      break;
+    case "hard":
+      numPairs = 25;
+      break;
+    case "extra-hard":
+      numPairs = 35;
+      break;
+    default:
+      numPairs = 15;
+  }
+
+  flippedCards = [];
+  isLocked = false;
+  score = 0;
+  updateScore();
+  stopTimer();
+
+  loadCardsAndStartGame(numPairs);
+
+  gameSound.play();
+  gameSound.volume = 0.03; // Set the volume to 50%
+
+  difficultyContainer.style.display = "none";
+  gameContainer.style.display = "block";
+}
+
+// Restart the game
+function restartGame() {
+  // winScreen.style.display = "none";
+  difficultyContainer.style.display = "block";
+  gameContainer.style.display = "none";
+}
+
+// Initialize the game
+function init() {
+  difficultyContainer.style.display = "block";
+  gameContainer.style.display = "none";
+}
+
+init();
